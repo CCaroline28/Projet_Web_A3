0fe3038dbed2f37f4bbee96b843cc48dc9c7bfb9
CCaroline28 <145038653+CCaroline28@users.noreply.github.com>
2026-06-18 10:09:38 +0200

    Update database.php

--- php/database.php
+++ php/database.php
@@ -226,7 +226,7 @@ function dbInsertInstallation($db, $data) {
 }
 
 #################################################################################STATISTIQUE###########################################################################
-  #Renvoie un tableau contenant l'id de la station et le type de ses prises (séparé par une virgule)
+    #Renvoie un tableau contenant l'id de la station et le type de ses prises (séparé par une virgule)
 function dbCounttypeprise($db,$dep){
     try {
         $request = "SELECT id_station,    GROUP_CONCAT(DISTINCT type_de_prise ORDER BY type_de_prise SEPARATOR ', ') AS types_de_prise
@@ -239,8 +239,8 @@ FROM (
 ) AS sub
 GROUP BY id_station;";
         $statement = $db->prepare($request);
-        $statement->execute();
-        $stmt->execute([":dep" => $dep . "%"]);
+        $statement->execute([
+            ":dep" => $dep . "%"]);
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     } catch (PDOException $exception) {
@@ -261,8 +261,8 @@ WHERE prise.consolidated_code_postal LIKE :dep
 GROUP BY station.implantation_station;
 ";
         $statement = $db->prepare($request);
-        $statement->execute();
-        $stmt->execute([":dep" => $dep . "%"]);
+        $statement->execute([
+            ":dep" => $dep . "%"]);
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     } catch (PDOException $exception) {
@@ -273,20 +273,20 @@ GROUP BY station.implantation_station;
 
 #Renvoie un tableau contenant le nb de point de charge  et son total dans la base
 # il nous faut un graphe avec en absisse les nb de points de charges et en ordonnée le total
-function dbCountimplantation($db, $dep){
+function dbCountnbpoints($db, $dep){
     try {
         $request = "SELECT nb_pdc, COUNT(*) AS total_de_pdc
 FROM (
     SELECT id_station, MAX(nbre_pdc) AS nb_pdc, COUNT(*) AS total_prise  # ça renvoie un tableau avec l'id de la statino et le nb de pdc 
     FROM prise
-    WHERE consolidated_code_postal LIKE :dep'
+    WHERE consolidated_code_postal LIKE :dep
     GROUP BY id_station
 ) AS sub
 GROUP BY nb_pdc
 ";
         $statement = $db->prepare($request);
-        $statement->execute();
-        $stmt->execute([":dep" => $dep . "%"]);
+        $statement->execute([
+            ":dep" => $dep . "%"]);
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     } catch (PDOException $exception) {
